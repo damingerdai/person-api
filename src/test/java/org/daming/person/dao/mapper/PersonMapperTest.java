57add7f602027c21c773df11e049dc700c12d1ae
明国宾 <mingguobin@live.com>
2019-01-16 21:46:07 +0800

    test: 对查询person进行单元测试

---
+++ src/test/java/org/daming/person/dao/mapper/PersonMapperTest.java
@@ -0,0 +1,27 @@
+package org.daming.person.dao.mapper;
+
+import org.daming.person.Application;
+import org.daming.person.pojo.doman.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class PersonMapperTest {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Test
+    public void get() {
+        Person person = personMapper.get("1");
+        assertNotNull(person);
+        System.out.println(person);
+    }
+
+}

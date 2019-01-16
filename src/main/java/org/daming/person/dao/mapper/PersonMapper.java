67d71c0055234f289f37073beb2373a402d8243d
明国宾 <mingguobin@live.com>
2019-01-16 21:23:11 +0800

    feat: 完成person的查询

--- src/main/java/org/daming/person/dao/mapper/PersonMapper.java
+++ src/main/java/org/daming/person/dao/mapper/PersonMapper.java
@@ -1,10 +1,13 @@
 package org.daming.person.dao.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.daming.person.pojo.doman.Person;
 
 @Mapper
 public interface PersonMapper {
 
-    Person get(String id);
+    @Select("SELECT id, `name`, surname, birthday, tellphone, province, city, `area`, address FROM person WHERE id = #{id}")
+    Person get(@Param("id")String id);
 }

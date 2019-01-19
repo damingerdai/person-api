5b00d22a22ea52a30627051b4ea33cb7ced9a376
明国宾 <mingguobin@live.com>
2019-01-19 16:25:49 +0800

    test: 提供对PersonService接口的测试，但是尚未完成

---
+++ src/test/java/org/daming/person/service/PersonServiceTest.java
@@ -0,0 +1,39 @@
+package org.daming.person.service;
+
+import org.daming.person.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class PersonServiceTest {
+
+    private PersonService personService;
+
+    @Test
+    public void get() {
+
+    }
+
+    @Test
+    public void update() {
+
+    }
+
+    @Test
+    public void add() {
+
+    }
+
+    @Test
+    public void delete() {
+
+    }
+
+
+
+}

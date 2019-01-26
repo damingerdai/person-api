1ceda4a4ad8155a64ffdf15a797d2316ecefd829
明国宾 <mingguobin@live.com>
2019-01-26 21:27:21 +0800

    test: 提供对tokenservice的单元测试

---
+++ src/test/java/org/daming/person/service/TokenServiceTest.java
@@ -0,0 +1,14 @@
+package org.daming.person.service;
+
+import org.daming.person.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TokenServiceTest {
+
+}

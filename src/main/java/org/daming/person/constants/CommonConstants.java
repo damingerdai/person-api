4dd47ca163c91898f3e7a6b57f353bb416b2c9a1
明国宾 <mingguobin@live.com>
2019-02-03 22:56:18 +0800

    feat: 添加常量类

---
+++ src/main/java/org/daming/person/constants/CommonConstants.java
@@ -0,0 +1,16 @@
+package org.daming.person.constants;
+
+public final class CommonConstants {
+
+    private CommonConstants() {
+
+    }
+
+    public static final String LOGGER_NAME_COMMON = "common.logger";
+
+    public static final String LOGGER_NAME_ERROR = "error.logger";
+
+    public static final String LOGGER_NAME_API = "api.logger";
+
+    public static final String LOGGER_NAME_JDBC = "jdbc.logger";
+}

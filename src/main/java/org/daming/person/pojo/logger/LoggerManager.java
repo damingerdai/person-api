f0955478a29052a651b9c0149505fa6c0d057c65
明国宾 <mingguobin@live.com>
2019-02-03 23:08:21 +0800

    feat: 添加常用的日志

--- src/main/java/org/daming/person/pojo/logger/LoggerManager.java
+++ src/main/java/org/daming/person/pojo/logger/LoggerManager.java
@@ -1,5 +1,6 @@
 package org.daming.person.pojo.logger;
 
+import org.daming.person.constants.CommonConstants;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,22 @@ import java.util.Map;
 public class LoggerManager {
 
     private Map<String, PersonLogger> cache;
+
+    public PersonLogger getCommonLogger() {
+        return getLogger(CommonConstants.LOGGER_NAME_COMMON);
+    }
+
+    public PersonLogger getErrorLogger() {
+        return getLogger(CommonConstants.LOGGER_NAME_ERROR);
+    }
+
+    public PersonLogger getApiLogger() {
+        return getLogger(CommonConstants.LOGGER_NAME_API);
+    }
+
+    public PersonLogger getJdbcLogger() {
+        return getLogger(CommonConstants.LOGGER_NAME_JDBC);
+    }
 
     public PersonLogger getLogger(String loggerName) {
         return cache.computeIfAbsent(loggerName, this::buildLogger);
